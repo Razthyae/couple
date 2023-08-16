ce8fcb763de9bbd2046bb468046251a60b92f1d8
Akash Kumar <a_kumar@poczta.fm>
2023-08-16 14:25:25 +0200

    Removed devmode button

--- pages/game.tsx
+++ pages/game.tsx
@@ -43,9 +43,7 @@ export default function Game() {
           <Dice />
           {/* <button className={styles.backToMenuBtn}>BoxShadow</button> */}
           
-          <button onClick={() => setDevMode(!devMode)} className={styles.devButton}>
-        Dev mode = {`${devMode}`}{" "}
-      </button>
+          
       <button className={styles.backButton} onClick={() => router.push('/')}><svg id="backButtonArrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg></button>
         </div>
       </main>
